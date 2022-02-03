a2c67609f73d22782a6875e4b8057ba7fed80a4b
realmarv <zahratehraninasab@gmail.com>
2022-02-03 10:11:04 +0330

    fix error

--- preprocess.sh
+++ preprocess.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 DATA_PATH='/content/potr/data/dataset'
-CONVERTER_PATH='/content/potr/data/h36_converter_txt_to_numpy.py'
+CONVERTER_PATH='/content/potr/data/h36_convert_txt_to_numpy.py'
 python3 ${CONVERTER_PATH} --data_path ${DATA_PATH} --output_path ${DATA_PATH}
